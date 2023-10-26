29a350814cf6dad0f7718b3d50cd4c121ffdc1be
Yanis540 <yanistabellout4@gmail.com>
2023-10-26 22:12:48 +0200

    [TME4]# patched a minor error

--- Java/src/com/paracamplus/ilp2/ilp2tme4/parser/ilpml/method2/ASTUnlessTransform.java
+++ Java/src/com/paracamplus/ilp2/ilp2tme4/parser/ilpml/method2/ASTUnlessTransform.java
@@ -34,7 +34,7 @@ public class ASTUnlessTransform implements IASTvisitor<IASTexpression, Void, Eva
         IASTfunctionDefinition [] functions = new IASTfunctionDefinition[iast.getFunctionDefinitions().length];
         int i = 0 ; 
         for ( IASTfunctionDefinition fd : iast.getFunctionDefinitions() ) 
-            functions[i] = fd;
+            functions[i] = (IASTfunctionDefinition)this.visit(fd,data);
         return factory.newProgram(
             functions, 
             iast.getBody().accept(this, data)
